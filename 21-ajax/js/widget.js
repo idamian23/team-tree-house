322a6c8994c8e7eef63a84477e237246572e9a4e
idamian23 <damian.ionut23@yahoo.ro>
2022-12-06 11:10:49 +0200

    ajax

---
+++ 21-ajax/js/widget.js
@@ -0,0 +1,45 @@
+// var xhr = new XMLHttpRequest()
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//         var employees = JSON.parse(xhr.responseText)
+//         var statusHTML = '<ul class="bulleted">'
+//         for (var i = 0; i < employees.length; i++) {
+//             if (employees[i].inoffice === true) {
+//                 statusHTML += '<li class="in">'
+//             } else {
+//                 statusHTML += '<li class="out">'
+//             }
+//             statusHTML += employees[i].name
+//             statusHTML += '</li>'
+//         }
+//         statusHTML += '</ul>'
+
+//         document.getElementById('employeeList').innerHTML = statusHTML
+//     }
+// }
+
+// xhr.open('GET', 'data/employees.json')
+// xhr.send()
+
+var roomRequest = new XMLHttpRequest()
+roomRequest.onreadystatechange = function () {
+    if (roomRequest.readyState === 4) {
+        var rooms = JSON.parse(roomRequest.responseText)
+        var statusHTML = '<ul class="rooms">'
+        for (var i = 0; i < rooms.length; i++) {
+            if (rooms[i].available === true) {
+                statusHTML += '<li class="empty">'
+            } else {
+                statusHTML += '<li class="full">'
+            }
+            statusHTML += rooms[i].room
+            statusHTML += '</li>'
+        }
+        statusHTML += '</ul>'
+
+        document.getElementById('roomList').innerHTML = statusHTML
+    }
+}
+
+roomRequest.open('GET', 'data/rooms.json')
+roomRequest.send()
